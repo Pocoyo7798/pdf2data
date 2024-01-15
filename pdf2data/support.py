d37558e5ef2649793f866b4d6c638417ec9341c6
Pocoyo7798 <daniel.pereira.costa@tecnico.ulisboa.pt>
2024-01-15 15:48:31 +0000

    creation support.py

---
+++ pdf2data/support.py
@@ -0,0 +1,59 @@
+from typing import Any, Dict, List, Optional
+from bs4 import BeautifulSoup as bs
+import os
+
+def find_term_in_list(reference: List[str], term: str) -> List[str]:
+    """_summary_
+
+    Parameters
+    ----------
+    reference : List[str]
+        List containing info about a reference
+    term : str
+        Term to be found in the reference list
+
+    Returns
+    -------
+    List[str]
+        List containing the terms found
+    """
+    all_term: List[str] = reference.find_all(term)
+    if len(all_term) > 0:
+        term_list: List[str] = []
+        for term in all_term:
+            volume_text = term.text
+            term_list.append(volume_text)
+        return term_list
+    else:
+        return ['Nothing Found']
+    
+def list_into_bs_format(list: List[str], file_format: str = "lxml") -> Any:
+    """_summary_
+
+    Parameters
+    ----------
+    list : List[str]
+        List of strings to be transformed
+    file_format : str, optional
+       format type of the output, by default "lxml"
+
+    Returns
+    -------
+    Any
+        string in the specfied format
+    """
+    string_object: str = "".join(str(data) for data in list)
+    bs_list: Any = bs(string_object, file_format)
+    return bs_list
+
+def get_doc_list(file_path: str, file_format: str):
+    """Obtain a the name of the documents inside a list inside a paste
+    (file_path) terminating in an specified file formar (file_format) like
+    .pdf or . png"""
+    all_docs = []
+    # Go through every file inside the desired file_path
+    for entry in os.listdir(file_path):
+        # Verifies the final extension of the file
+        if entry.endswith(file_format):
+            all_docs = all_docs + [entry]
+    return all_docs
