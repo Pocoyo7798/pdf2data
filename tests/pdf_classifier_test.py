c9b3dcd62802a3b901f6d54fde9e6aaa9dbce893
Pocoyo7798 <daniel.pereira.costa@tecnico.ulisboa.pt>
2024-01-16 14:20:18 +0000

    creation of pdf_classifier_test.py

---
+++ tests/pdf_classifier_test.py
@@ -0,0 +1,21 @@
+import fitz
+import importlib_resources
+from fitz import Document
+
+from pdf2data.pdf_classifier import PDF_Classifier
+
+
+def pdf_classifier_test():
+    """Test the PDF_Classifier class"""
+    text_file_path: str = str(
+        importlib_resources.files("pdf2data") / "resources" / "test.pdf"
+    )
+    scanned_file_path: str = str(
+        importlib_resources.files("pdf2data") / "resources" / "test_scanned.pdf"
+    )
+    text_doc: Document = fitz.open(text_file_path)
+    scanned_doc: Document = fitz.open(scanned_file_path)
+    classifier_text: PDF_Classifier = PDF_Classifier(document=text_doc)
+    classifier_scanned: PDF_Classifier = PDF_Classifier(document=scanned_doc)
+    assert classifier_text.has_text is True
+    assert classifier_scanned.has_text is False
