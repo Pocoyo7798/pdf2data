b8353b8294b4ff53933e19f94576a986d3183ba0
Pocoyo7798 <daniel.pereira.costa@tecnico.ulisboa.pt>
2024-01-22 14:27:11 +0000

    creation of textextractor cli

---
+++ pdf2data/cli/text_extractor.py
@@ -0,0 +1,65 @@
+import os
+from typing import Any, Dict, List, Optional
+
+import click
+
+from pdf2data.mask import LayoutParser
+from pdf2data.text import TextFileGenerator, TextExtractor
+from pdf2data.support import get_doc_list
+
+@click.command()
+@click.argument("input_folder", type=str)
+@click.option(
+    "--output_folder",
+    default=None,
+    help="Output folder path",
+)
+@click.option(
+    "--type",
+    default="layoutparser",
+    help="type of the text extractor, available: ['layoutparser', 'cermine', 'minersix']",
+)
+
+def text_extractor(input_folder: str, output_folder: Optional[str], type:str) -> None:
+    if output_folder is None:
+        output_folder = input_folder
+    possible_types = set(["layoutparser", "cermine", "minersix"])
+    if type not in possible_types:
+        raise AttributeError(f"{type} is not a available type, try one of the following: {possible_types}")
+    if type == "layoutparser":
+        mask = LayoutParser(model="PubLayNet_mask_rcnn_X_101_32x8d_FPN_3x")
+        mask.model_post_init(None)
+        docs = get_doc_list(input_folder, ".pdf")
+    else:
+        generator = TextFileGenerator(input_folder=input_folder, output_folder=output_folder)
+        generator.model_post_init(None)
+        if type == "Cermine":
+            generator.pdf_to_cermxml()
+            docs = get_doc_list(output_folder, ".cermxml")
+        elif type == "minersix":
+            generator.pdf_to_miner("txt")
+            docs = get_doc_list(output_folder, ".txt")
+    for doc in docs:
+        file_name = os.path.splitext(doc)[0]
+        if type == "layoutparser":
+            file_path = f"{input_folder}/{doc}"
+            extractor = TextExtractor(input_file=file_path, output_folder=output_folder)
+            layout = mask.get_layout(file_path, 0.5)
+            extractor.extract_layoutparser(f"{file_name}_text", layout)
+        else:
+            file_path = f"{output_folder}/{doc}"
+            extractor = TextExtractor(input_file=file_path, output_folder=output_folder)
+            if type == "cermine":
+                extractor.extract_cermine(f"{file_name}_text")
+            elif type == "minersix":
+                extractor.extract_txt(f"{file_name}_text")
+
+
+def main():
+    text_extractor()
+
+
+if __name__ == "__main__":
+    main()
+    
+
